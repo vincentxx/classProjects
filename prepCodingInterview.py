ae949cdec4afa8164902c19410d1a54e2de79289
vuqt1 <vuqt1@ics.uci.edu>
2019-01-14 05:09:21 -0800

    prep for coding interview

---
+++ prepCodingInterview.py
@@ -0,0 +1,49 @@
+Objective: Review and remember ONLY important practices of Data Structure and Algorithms for Coding Interviews
+
+Note concepts, Pseudocode, Python, C++.
+
+"Coding in timing"
+
+################################################## I
+
+//Binary Search of ordered List
+
+//Interpolation Search of ordered List
+
+//Insertion Sort
+
+//Shell Sort
+
+//Heapsort on the Heap data structure
+
+//Distribution sort - Bucket Sort
+
+//Radix Sort
+
+//Quick Sort
+
+//Merge Sort
+
+//Divide and Conquer technique
+The master theorem:
+
+//Matrix Multiplication
+
+//Skyline problem
+
+//Dynamic Programming applications*
+Idea: using more spaces to store the subproblems instead of paying processing of repeat problems (as recursive ways). Bottom-up.
+
+Very frequently asked in interviews:
+
+    //Common Substrings, Subsequences
+    //Optimal Binary Search
+    //Nim 
+    //Fibonacci
+    //Knapsack
+    //
+
+//Hash
+
+//
+
